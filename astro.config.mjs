9e2d6a3676ef2778d55da63808e71b445f881355
Mark Ahrens <mark@mytungsten.net>
2024-02-24 21:13:40 -0600

    Adding sitemap.xml and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://avgeek.mytungsten.net',
+  integrations: [react(), sitemap()]
 });
