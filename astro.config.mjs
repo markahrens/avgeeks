af0faf60d1f1e757632ae189c4f66d111af32ee5
Mark Ahrens <mark@mytungsten.net>
2024-03-13 21:19:36 -0500

    Adding netlify adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://avgeek.mytungsten.net',
-  integrations: [react(), sitemap()]
+  integrations: [react(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
