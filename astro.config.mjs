6a2e91c208ee902570e447a0b0f6326ab0bdf5b5
Mark Ahrens <mark@mytungsten.net>
2024-06-26 20:18:12 -0500

    Updating to CF adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from "@astrojs/sitemap";
-
-import netlify from "@astrojs/netlify";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://avgeek.mytungsten.net',
   integrations: [react(), sitemap()],
   output: "server",
-  adapter: netlify()
+  adapter: cloudflare()
 });
